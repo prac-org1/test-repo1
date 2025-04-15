90315f9620c0782bfbd90bea57700e1fc85f2fc2
Samarth DevTools <samarthm@devtools.in>
2025-04-15 11:09:29 +0530

    Create functions.ts

---
+++ functions.ts
@@ -0,0 +1,11 @@
+//First function
+function hello():string{
+    return "Hello";
+}
+console.log(hello());
+
+//passing single parameter to a function
+function hi(name:string):string{
+    return "hi "+ name;
+}
+console.log(hi("Samarth"));
